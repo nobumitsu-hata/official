7ee216370ea43573e435062a8b2e7d4009205696
秦頌光 <first1freedom@gmail.com>
2017-08-24 11:43:29 +0900

    index.phpを編集 デフォルトの状態にしました。

--- index.php
+++ index.php
@@ -1,2 +1,24 @@
 <?php
+    // 環境変数関連
+    define('ROOT', __DIR__);
+    define('URL', 'http://' . $_SERVER["HTTP_HOST"]);
+    $tag_arr = array('app');
+    $request = $_SERVER['REQUEST_URI'];
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+<?php
+    require_once(ROOT . '/head.php');
+?>
+</head>
+<body>
+    <app></app>
+    <?php foreach($tag_arr as $key => $value) { ?>
+        <script src="<?php echo URL_THEME . '/tag/' . $value . '.tag?20170824'; ?>" type="riot/tag"></script>
+    <?php } ?>
+    <script>
+        riot.mount('app')
+    </script>
+</body>
+</html>
