b2b3743b3b4ada90cf6ce56223fda54e7c575064
秦頌光 <first1freedom@gmail.com>
2017-08-24 13:51:18 +0900

    [add] app-name.tag/app-footer.tag

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     // 環境変数関連
     define('ROOT', __DIR__);
     define('URL', 'http://' . $_SERVER["HTTP_HOST"]);
-    $tag_arr = array('app');
+    $tag_arr = array('app', 'app-name', 'app-footer');
     $request = $_SERVER['REQUEST_URI'];
 ?>
 <!DOCTYPE html>
@@ -15,7 +15,7 @@
 <body>
     <app></app>
     <?php foreach($tag_arr as $key => $value) { ?>
-        <script src="<?php echo URL_THEME . '/tag/' . $value . '.tag?20170824'; ?>" type="riot/tag"></script>
+        <script src="<?php echo URL . '/tag/' . $value . '.tag?20170824'; ?>" type="riot/tag"></script>
     <?php } ?>
     <script>
         riot.mount('*')
--- tag/app.tag
+++ tag/app.tag
@@ -1,8 +1,11 @@
 <app>
     <div>
-
+        <name></name>
+        <div>I am a front-end developer.</div>
+        <app-footer></app-footer>
     </div>
     <style>
+
     </style>
     <script>
     </script>
