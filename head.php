c5c2d2b987a58a0ca02481d12b32089bba654747
秦頌光 <first1freedom@gmail.com>
2017-08-25 01:03:19 +0900

    [update] ogp設定

--- head.php
+++ head.php
@@ -2,7 +2,7 @@
     # head.php
 ?>
     <meta charset="UTF-8">
-    <!-- <title><?php echo $meta['title']; ?></title>
+    <title><?php echo $meta['title']; ?></title>
     <meta name="description" content="<?php echo $meta['description']; ?>">
     <meta name="keywords" content="<?php echo $meta['keywords']; ?>">
     <meta name="author" content="<?php echo $meta['author']; ?>">
@@ -19,12 +19,6 @@
     <meta name="twitter:creator" content="<?php echo $meta['twitter']['creator']; ?>">
     <meta name="twitter:title" content="<?php echo $meta['twitter']['title']; ?>">
     <meta name="twitter:description" content="<?php echo $meta['twitter']['description']; ?>">
-    <meta name="twitter:image" content="<?php echo $meta['twitter']['image']; ?>"> -->
-    <!-- meta link -->
-    <!-- <link rel="author" href="<?php echo $meta['google']['account']; ?>">
-    <link rel="publisher" href="<?php echo $meta['google']['account']; ?>"> -->
+    <meta name="twitter:image" content="<?php echo $meta['twitter']['image']; ?>">
     <link rel="stylesheet" href="<?php echo URL . '/css/style.css'; ?>">
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     <script src="https://cdn.jsdelivr.net/riot/3.4.4/riot+compiler.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/riot-route/3.1.1/route.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.1/TweenMax.min.js"></script>
--- index.php
+++ index.php
@@ -4,6 +4,24 @@
     define('URL', 'http://' . $_SERVER["HTTP_HOST"]);
     $tag_arr = array('app', 'app-name', 'app-footer');
     $request = $_SERVER['REQUEST_URI'];
+    // OGP設定
+    $meta['title']                  = 'nobumitsu hata';
+    $meta['description']            = 'プログラマー秦頌光のオフィシャルサイト';
+    $meta['keywords']               = 'nobumitsu,hata,nobumitsu hata,秦頌光,秦 頌光,秦,頌光';
+    $meta['author']                 = '秦 頌光';
+    $meta['ogp']['title']           = 'nobumitsu hata';
+    $meta['ogp']['type']            = 'website';
+    $meta['ogp']['description']     = 'プログラマー秦頌光のオフィシャルサイト';
+    $meta['ogp']['url']             = URL;
+    $meta['ogp']['image']           = 'http://nobumitsu-hata.com/img/ogp.png';
+    $meta['ogp']['site_name']       = 'nobumitsu hata';
+    $meta['ogp']['email']           = 'first1freedom@gmail.com';
+    $meta['twitter']['card']        = 'summary_large_image';
+    $meta['twitter']['site']        = '@ww_uktki';
+    $meta['twitter']['creator']     = '@ww_uktki';
+    $meta['twitter']['title']       = 'nobumitsu hata';
+    $meta['twitter']['description'] = 'プログラマー秦頌光のオフィシャルサイト';
+    $meta['twitter']['image']       = 'http://nobumitsu-hata.com/img/ogp.png';
 ?>
 <!DOCTYPE html>
 <html>
