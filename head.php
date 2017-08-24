54a568440b2f06f6e00a3f629002a3a05ca327cc
秦頌光 <first1freedom@gmail.com>
2017-08-24 12:01:39 +0900

    [add] head.php

---
+++ head.php
@@ -0,0 +1,30 @@
+<?php
+    # head.php
+?>
+    <meta charset="UTF-8">
+    <!-- <title><?php echo $meta['title']; ?></title>
+    <meta name="description" content="<?php echo $meta['description']; ?>">
+    <meta name="keywords" content="<?php echo $meta['keywords']; ?>">
+    <meta name="author" content="<?php echo $meta['author']; ?>">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no,shrink-to-fit=no">
+    <meta property="og:title" content="<?php echo $meta['ogp']['title']; ?>">
+    <meta property="og:type" content="<?php echo $meta['ogp']['type']; ?>">
+    <meta property="og:description" content="<?php echo $meta['ogp']['description']; ?>">
+    <meta property="og:url" content="<?php echo $meta['ogp']['url']; ?>">
+    <meta property="og:image" content="<?php echo $meta['ogp']['image']; ?>">
+    <meta property="og:site_name" content="<?php echo $meta['ogp']['site_name']; ?>">
+    <meta property="og:email" content="<?php echo $meta['ogp']['email']; ?>">
+    <meta name="twitter:card" content="<?php echo $meta['twitter']['card']; ?>">
+    <meta name="twitter:site" content="<?php echo $meta['twitter']['site']; ?>">
+    <meta name="twitter:creator" content="<?php echo $meta['twitter']['creator']; ?>">
+    <meta name="twitter:title" content="<?php echo $meta['twitter']['title']; ?>">
+    <meta name="twitter:description" content="<?php echo $meta['twitter']['description']; ?>">
+    <meta name="twitter:image" content="<?php echo $meta['twitter']['image']; ?>"> -->
+    <!-- meta link -->
+    <!-- <link rel="author" href="<?php echo $meta['google']['account']; ?>">
+    <link rel="publisher" href="<?php echo $meta['google']['account']; ?>"> -->
+    <link rel="stylesheet" href="<?php echo URL . '/style.css'; ?>">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <script src="https://cdn.jsdelivr.net/riot/3.4.4/riot+compiler.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/riot-route/3.1.1/route.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.1/TweenMax.min.js"></script>
